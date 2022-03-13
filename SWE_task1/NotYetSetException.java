1b99f19bb2ca4aba655130c0f7833d533d0b7921
Mohamed Twfik <69367912+Mohamed-Twfik@users.noreply.github.com>
2022-03-13 23:56:10 +0200

    Add files via upload

---
+++ SWE_task1/NotYetSetException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
+
+public class NotYetSetException extends Exception {
+
+    public NotYetSetException(String s) {
+        super(s);
+    }
+}
